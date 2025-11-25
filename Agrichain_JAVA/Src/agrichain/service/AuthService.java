338b43a1a0a928382e81b9f78724e56357f4e423
Sahib2306 <lpu.sahib@gmail.com>
2025-11-26 00:38:44 +0530

    code updated acc to jdbc

--- Agrichain_JAVA/Src/agrichain/service/AuthService.java
+++ Agrichain_JAVA/Src/agrichain/service/AuthService.java
@@ -1,26 +1,62 @@
 package agrichain.service;
 
-public class AuthService {
+import java.sql.*;
+import agrichain.data.DBConnection;
 
-    // Hardcoded sample users for login (temporary until JDBC)
-    public boolean login(String id, String pass, String role) {
+public class AuthService{
 
-        // sample manual login records
-        if(id.equals("1001") && pass.equals("pass") && role.equalsIgnoreCase("FARMER")) {
-            return true;
-        }
-        if(id.equals("2001") && pass.equals("1234") && role.equalsIgnoreCase("BUYER")) {
-            return true;
-        }
-        if(id.equals("3001") && pass.equals("1111") && role.equalsIgnoreCase("CONSUMER")) {
-            return true;
-        }
-        if(id.equals("4001") && pass.equals("999") && role.equalsIgnoreCase("TRANSPORTER")) {
-            return true;
-        }
+    // checking the login credentials from database
+    public boolean login(String id, String pass, String role){
+        boolean isValid = false;
 
-        return false;  // for wrong credentials
+        try{
+            // getting the connection from databse 
+            Connection conn  = DBConnection.getConn();
+
+            // PreparedStatement is used when we want to send values (?)
+            // ? question mark will be replaced by the values 
+            PreparedStatement ps = conn.prepareStatement("Select * from users where userId=? AND password=? AND role=?");
+
+            ps.setString(1,id); // puts the id into ?
+            ps.setString(2, pass);
+            ps.setString(3, role);
+
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()){ // if any row matches the credentials then the login will work 
+                isValid = true; 
+            }
+
+            conn.close();
+
+        }
+        catch(Exception e){
+            System.out.println("Login Failed!");
+        }
+        return isValid;
     }
 
-    // register removed since no file handling
+    public void register(String id, String name , String pass, String role){
+
+        try{
+            Connection conn = DBConnection.getConn();
+            PreparedStatement ps = conn.prepareStatement("insert into users values(?,?,?,?)");
+
+            ps.setString(1, id);
+            ps.setString(2, pass);
+            ps.setString(3, name);
+            ps.setString(4, role);
+
+            ps.executeUpdate(); // it is used for inserting data , updating the data , deleting the data
+            conn.close();
+
+
+
+
+        }
+        catch(Exception e){
+            System.out.println("Registration Failed!");
+        }
+    }
 }
